5ce0824b20bfacb6412a46309087fe79b658829c
vivian <vivian@viviandeMacBook-Air.local>
2023-11-19 02:08:19 +1100

    added feed page and buttons components

---
+++ soso-master/src/components/postButtons.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Button, Text, useTheme } from "@ui-kitten/components";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { FontAwesome5, Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+
+const PostButtons = () => {
+    // const LikeIcon = <Feather name="thumbs-up" size={16} color="black"/>;
+    // const CommentIcon = (props) => <FontAwesome5 name="comment" size={16} color="black" {...props} />;
+    // const ShareIcon = (props) => <MaterialCommunityIcons name="share-outline" size={16} color="black" {...props} />;
+
+    // const theme = useTheme();
+    return (
+        <View style={styles.footerContainer}>
+            <TouchableOpacity style={styles.button}>
+              <Feather name="thumbs-up" size={16} color="black"/>
+              <Text style={styles.button_text}>Like</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <FontAwesome5 name="comment" size={16} color="black"/>
+              <Text style={styles.button_text}>Comment</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <MaterialCommunityIcons name="share-outline" size={16} color="black"/>
+              <Text style={styles.button_text}>Share</Text>
+            </TouchableOpacity>
+          </View>
+  );
+};
+
+export default PostButtons;
+
+const styles = StyleSheet.create({
+    footerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+    },
+    button: {
+        flex:1,
+        flexDirection:"row",
+        justifyContent:'center',
+        marginTop:30,
+        // borderWidth:1,
+        // borderColor:"#4D4352",
+    },
+    button_text: {
+        color: "#4D4352",
+        fontSize: 15,
+        fontWeight: 400,
+        marginLeft:10,
+    }
+});
--- soso-master/src/screens/feed/feed.jsx
+++ soso-master/src/screens/feed/feed.jsx
@@ -1,20 +1,53 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { FontAwesome5, Entypo } from '@expo/vector-icons';
-import { Layout, Tab, TabView, Text, List, ListItem, TouchableOpacity } from "@ui-kitten/components";
+import { StyleSheet, View, Image } from "react-native";
+import { FontAwesome5, Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Avatar, Tab, TabView, Text, Button, Card } from "@ui-kitten/components";
 import { TopNavigation } from '@ui-kitten/components';
+import PostButtons from "../../components/postButtons";
 // import BackIcon from '../backArrow';
 
 const Feed = ( {navigation} ) => {
+  // const LikeIcon = <Feather name="thumbs-up" size={16} color="black" />
+  // const CommentIcon = <FontAwesome5 name="comment" size={16} color="black" />;
+  // const ShareIcon = <MaterialCommunityIcons name="share-outline" size={16} color="black" />
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.topContainer}>
+      <View style={styles.topContainer}> 
         <Text category="h3" status="primary">
           Feed
         </Text>
         <View style={styles.iconsContainer}>
-          <FontAwesome5 name="facebook-messenger" size={24} color="black"/>
+          <FontAwesome5 name="facebook-messenger" size={24} color="black" onPress={() => navigation.navigate('Chat')}/>
           <Entypo name="new-message" size={24} color="black" onPress={() => navigation.navigate('NewPost')}/>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.contentContainer}>
+          <View style={ {flexDirection:"row"}}>
+            <Avatar
+              source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Zoe_Kravitz_2020_dvna_studio.jpg' }} 
+              style={styles.avatar}
+              size="medium"
+            />
+            <View style={styles.textContainer}>
+              <Text category='s1'>Shrek</Text>
+              <Text appearance='hint'>few minutes ago</Text>
+            </View>
+          </View>
+          <MaterialCommunityIcons name="information-outline" size={30} color="black" />
+        </View>
+        <View style={styles.postArea}>
+          <Image
+            style={{ width: "100%", height: 192 }}
+            source={{ uri: "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"}}
+          />
+          <Text style={{ marginHorizontal: 15, marginTop: 10}} category='p1'>
+            Went back to the swamp with Fiona. It was fun!
+          </Text>
+          <PostButtons/>
         </View>
       </View>
     </View>
@@ -28,7 +61,7 @@ const styles = StyleSheet.create({
     // flexDirection: 'row', 
     justifyContent:"flex-start",
     flex: 1,
-    paddingHorizontal: 20, 
+    paddingHorizontal: 15, 
     backgroundColor: "#fff",
   },
 
@@ -43,5 +76,33 @@ const styles = StyleSheet.create({
     justifyContent:"space-between",
     width: 60,
     alignItems:"center",
+  },
+  card: {
+    margin: 16,
+    borderRadius: 8,
+  },
+  headerImage: {
+    height: 200,
+  },
+
+  contentContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    justifyContent:"space-between",
+  },
+  textContainer: {
+    marginLeft: 8,
+  },
+
+  postArea:{
+    borderWidth: 0,
+    borderRadius: 15,
+    overflow:"hidden",
+    // shadowColor: '#000',
+    // shadowOffset: { width: 0, height: 10 },
+    // shadowOpacity: 0.1,
+    // shadowRadius: 5,
+    // backgroundColor: '#fff'
   },
 });
