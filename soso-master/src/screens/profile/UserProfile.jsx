e47e1c54c620fc26f99f53c1b39ba0005beb13be
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-11-20 15:07:01 +1100

    init file

---
+++ soso-master/src/screens/profile/UserProfile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  ScrollView,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  TextInput,
+} from "react-native";
+import { BackAction } from "../../components/backAction";
+import { ScreenNormalView, ScreenView } from "../../components/CustomView";
+import {
+  useTheme,
+  Text,
+  Input,
+  Layout,
+  Card,
+  Button,
+  TopNavigation,
+  Icon,
+} from "@ui-kitten/components";
+import { Iconify } from "react-native-iconify";
