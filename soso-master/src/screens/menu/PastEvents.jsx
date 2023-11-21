eaeecddeccea0e8b2a651700bff525b50dd258e3
EliseLiu999 <eliseliulsy@gmail.com>
2023-11-22 10:25:00 +1100

    saved & past event done

--- soso-master/src/screens/menu/PastEvents.jsx
+++ soso-master/src/screens/menu/PastEvents.jsx
@@ -12,14 +12,45 @@ import {
   Avatar,
   useTheme,
   Text,
+  Button,
   Input,
   Layout,
   Card,
   TopNavigation,
 } from "@ui-kitten/components";
 import { Iconify } from "react-native-iconify";
+import { EventCard } from "../events/EventCard";
+import TextDivider from "../../components/TextDivider";
+
+const eventDetails = [
+  {
+    title: "Who's That Kangaroo",
+    dateTime: "WED, 28 OCT • 04:30 PM",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum.",
+    locationName: "Taronga Zoo",
+    imageUri: require("../../assets/images/SearchResultsImage1.png"),
+  },
+  {
+    title: "Real Life Roo Sighting",
+    dateTime: "WED, 28 OCT • 07:00 PM",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum.",
+    locationName: "Dharawal National Park",
+    imageUri: require("../../assets/images/SearchResultsImage2.png"),
+  },
+  {
+    title: "Whinnie The Roo Musical",
+    dateTime: "THU, 29 OCT • 05:00 PM",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu magna vehicula diam pulvinar dictum.",
+    locationName: "Sydney Opera House",
+    imageUri: require("../../assets/images/SearchResultsImage3.png"),
+  },
+];
 
 const PastEvents = ({ navigation, route }) => {
+  const [filteredEvents, setFilteredEvents] = React.useState(eventDetails);
   return (
     <ScreenNormalView>
       <TopNavigation
@@ -34,8 +65,11 @@ const PastEvents = ({ navigation, route }) => {
         alignment="start"
       />
       <ScrollView>
-        <View>
-          <Text>PastEvents</Text>
+        <View style={styles.content}>
+          {filteredEvents.map((event, index) => (
+            <EventCard key={index} event={event} />
+          ))}
+          <TextDivider text="End of Past Events" />
         </View>
       </ScrollView>
     </ScreenNormalView>
@@ -43,3 +77,8 @@ const PastEvents = ({ navigation, route }) => {
 };
 
 export default PastEvents;
+const styles = StyleSheet.create({
+  content: {
+    margin: 20,
+  },
+});
