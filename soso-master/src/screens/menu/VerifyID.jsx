4bf27956a10930ebdcf97170996051d5b26651d6
ItsLame <37344470+ItsLame@users.noreply.github.com>
2023-11-21 14:26:49 +1100

    menu verify id page 1

--- soso-master/src/screens/menu/VerifyID.jsx
+++ soso-master/src/screens/menu/VerifyID.jsx
@@ -16,6 +16,7 @@ import {
   Layout,
   Card,
   TopNavigation,
+  Button,
 } from "@ui-kitten/components";
 import { Iconify } from "react-native-iconify";
 
@@ -33,11 +34,59 @@ const MenuVerifyID = ({ navigation, route }) => {
         }
         alignment="start"
       />
-      <ScrollView>
-        <View>
-          <Text>MenuVerifyID</Text>
+      {/* <ScrollView> */}
+      <ScreenView>
+        <View style={{ gap: 22, marginTop: 20 }}>
+          <View>
+            <Text category="h4" status="primary">
+              Why verify your account?
+            </Text>
+            <Text category="p1">
+              In an effort to make SoSo a safer community, we recommend that you
+              verify your identity. Your data will be deleted once verified, and
+              you will be able to gain a few perks as well!
+            </Text>
+          </View>
+          <View style={{ gap: 35 }}>
+            <View style={{ gap: 10 }}>
+              <View>
+                <Text category="h5">Access to all events</Text>
+                <Text category="p1">
+                  You will be able to fund and register events that are
+                  restricted for verified users only.
+                </Text>
+              </View>
+              <View>
+                <Text category="h5">Restrict events</Text>
+                <Text category="p1">
+                  Similar to how you gain access to all events, once verified,
+                  you will be able to configure the restriction for your event.
+                </Text>
+              </View>
+              <View>
+                <Text category="h5">Safer community</Text>
+                <Text category="p1">
+                  Verified identity ensures other users that you are a real
+                  person and vice-versa.
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={{ gap: 10 }}>
+            <Button>Verify</Button>
+            <Button
+              appearance="ghost"
+              status="basic"
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              Cancel
+            </Button>
+          </View>
         </View>
-      </ScrollView>
+      </ScreenView>
+      {/* </ScrollView> */}
     </ScreenNormalView>
   );
 };
